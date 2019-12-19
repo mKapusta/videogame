f098a64dfac6fc41576a7436d277b2a920975b90
mKapusta <kapusta.maxime@gmail.com>
2019-12-19 16:58:19 +0100

    Initial Data

---
+++ V2__add_data.sql
@@ -0,0 +1,14 @@
+INSERT INTO TYPE_JEU (NOM) VALUES ('Jeu de rôle');
+INSERT INTO TYPE_JEU (NOM) VALUES ('Jeu de tir');
+INSERT INTO TYPE_JEU (NOM) VALUES ('Stratégie');
+INSERT INTO TYPE_JEU (NOM) VALUES ('Combat');
+
+
+INSERT INTO JEU_VIDEO (NOM, DESCRIPTION, DATE, PRIX, NOTE, ID_TYPE_JEU)
+  VALUES('The Witcher 3', 'Les aventures de Geralt de Riv', '19/05/2015', 30, 10, 1);
+INSERT INTO JEU_VIDEO (NOM, DESCRIPTION, DATE, PRIX, NOTE, ID_TYPE_JEU)
+  VALUES('DOOM', 'Un FPS, un vrai!', '13/05/2016', 10, 7, 2);
+INSERT INTO JEU_VIDEO (NOM, DESCRIPTION, DATE, PRIX, NOTE, ID_TYPE_JEU)
+  VALUES('Age of Empires', 'Choisissez votre nation, et menez là à la victoire', '15/10/1997', 0, 9, 3);
+INSERT INTO JEU_VIDEO (NOM, DESCRIPTION, DATE, PRIX, NOTE, ID_TYPE_JEU)
+  VALUES('Super Smash Bros Ultimate', 'Choose your character!', '07/12/2019', 60, 10, 4);
